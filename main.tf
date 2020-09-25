7b8eef5e2be1370b3b99696e71b79aeb89a5ef24
Facundo Gauna <facundo@boxboat.com>
2020-09-25 08:02:42 -0700

    formatting.

--- main.tf
+++ main.tf
@@ -27,11 +27,11 @@ resource "azurerm_resource_group" "resource_group" {
 module "virtual_network" {
   source = "./modules/virtual_network"
 
-app_name                   = local.app_name
-  resource_group       = azurerm_resource_group.resource_group.name
-  environment          = var.environment
-  location             = var.location
-  address_space = "10.0.0.0/16"
+  app_name            = local.app_name
+  resource_group      = azurerm_resource_group.resource_group.name
+  environment         = var.environment
+  location            = var.location
+  address_space       = "10.0.0.0/16"
   rancher_subnet_cidr = "10.0.2.0/24"
 
   tags = var.tags
@@ -54,17 +54,25 @@ module "mysql" {
 module "web_tier" {
   source = "./modules/web_tier"
 
-  tags                       = var.tags
-  app_name                   = local.app_name
-  resource_group             = azurerm_resource_group.resource_group.name
-  environment                = var.environment
-  location                   = var.location
-  database_connection_string = "mysql://${var.mysql_admin_username}@${module.mysql.server_name}:${var.mysql_admin_password}@tcp(${module.mysql.fqdn}:3306)/${local.database_name}?tls=true"
-  rancher_hostname           = var.rancher_hostname
-  instances                  = 2
-  virtual_network_name = module.virtual_network.name
-  rancher_subnet_name = module.virtual_network.rancher_subnet_name
+  tags                           = var.tags
+  app_name                       = local.app_name
+  resource_group                 = azurerm_resource_group.resource_group.name
+  environment                    = var.environment
+  location                       = var.location
+  database_connection_string     = "mysql://${var.mysql_admin_username}@${module.mysql.server_name}:${var.mysql_admin_password}@tcp(${module.mysql.fqdn}:3306)/${local.database_name}?tls=true"
+  rancher_hostname               = var.rancher_hostname
+  instances                      = 2
+  virtual_network_name           = module.virtual_network.name
+  rancher_subnet_name            = module.virtual_network.rancher_subnet_name
   virtual_network_resource_group = azurerm_resource_group.resource_group.name
 
   vm_admin_username = var.vm_admin_username
 }
+
+# resource "azurerm_mysql_firewall_rule" "mysql_allow_rancher_server" {
+#   name                = "rancher_server"
+#   resource_group_name = azurerm_resource_group.example.name
+#   server_name         = azurerm_mysql_server.example.name
+#   start_ip_address    = "0.0.0.0"
+#   end_ip_address      = "0.0.0.0"
+# }
