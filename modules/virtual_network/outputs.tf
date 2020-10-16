b1e045b1bf35b9f2afba8ca367267c686de5d20d
Facundo Gauna <fgauna12@gmail.com>
2020-10-16 06:51:12 -0700

    fixing bad reference

--- modules/virtual_network/outputs.tf
+++ modules/virtual_network/outputs.tf
@@ -6,10 +6,6 @@ output "rancher_subnet_id" {
     value = azurerm_subnet.rancher_subnet.id
 }
 
-output "azure_bastion_subnet_id" {
-    value = azurerm_subnet.azure_bastion_subnet.id
-}
-
 output "bastion_subnet_id" {
     value = azurerm_subnet.bastion_subnet.id
 }
